b84ddc1281d99350efcd624025fe6580099c42e5
JustHo <zalop3128@kakao.com>
2025-08-21 05:48:38 +0900

    [FEAT] 소셜로그인, Cookie  페이지 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import JoinPage from "./pages/JoinPage";
 import LoginPage from "./pages/LoginPage";
+import CookiePage from "./pages/CookiePage";
 
 import './App.css'
 
@@ -12,9 +13,10 @@ function App() {
       <Routes>
         <Route path="/join" element={<JoinPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/cookie" element={<CookiePage />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App
